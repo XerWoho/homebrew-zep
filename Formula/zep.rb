ad1b9cd59cd463c5ad4d8f776c9670c1624ae2d6
xerrs. <113448963+XerWoho@users.noreply.github.com>
2026-01-11 22:38:34 +0200

    Update zep.rb to version 1.0.2

--- Formula/zep.rb
+++ Formula/zep.rb
@@ -4,13 +4,13 @@ class Zep < Formula
   license "GPLv3"
 
   on_macos do
-    url "https://zep.run/releases/1.0.1/zep_x86_64-macos_1.0.1.tar.xz"
-    sha256 "21020e2d2bc84145b1e70e29bec917540cbeaeb8a1a1e82e57a30200694b3ae3"
+    url "https://zep.run/releases/1.0.2/zep_x86_64-macos_1.0.2.tar.xz"
+    sha256 "11671f4b6423f2e1ec023f211b8d9439c085dd8598b5bcda7fa2552cd798ae8a"
   end
 
   on_linux do
-    url "https://zep.run/releases/1.0.1/zep_x86_64-linux_1.0.1.tar.xz"
-    sha256 "5437049f8cceb2183ea0549d49e1198bd6331077f23ebefd720d85edf206f272"
+    url "https://zep.run/releases/1.0.2/zep_x86_64-linux_1.0.2.tar.xz"
+    sha256 "5bb76c0441a5f08a412ca3dd65209a1714bf04adb6ec57aea13a6e924e4307fe"
   end
 
   def install
@@ -20,7 +20,7 @@ class Zep < Formula
     def post_install
 		ohai "--- ZEP CONFIG REQUIRED ---"
 		puts " ==> Run 'zep setup' to configure zep"
-		puts " ==> Then run: zep zep install 1.0.1"
+		puts " ==> Then run: zep zep install 1.0.2"
 	end
 
   test do
